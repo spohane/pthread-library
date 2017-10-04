881a1123be6d43419f017b7c4ed69dc90bc40997
Joshua Kim <joshua@nbp-112-38.nbp.ruw.rutgers.edu>
2017-10-03 20:12:28 -0400

    Basic skeleton of the code

---
+++ my_pthread_t.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "my_pthread_t.h"
+
+
+int main(int argc, char ** argv)
+{
+
+
+  return 0;
+}
---
+++ my_pthread_t.h
@@ -0,0 +1,6 @@
+#ifndef _MY_PTHREAD_T_H_
+#define _MY_PTHREAD_T_H
+
+
+
+#endif
